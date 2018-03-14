68c6b839492aeb5bc48839d7f3ffcd8ca0fdd52c
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-03-14 16:14:17 +0300

    Reactive Forms Part 1

---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,11 @@
+export class Feedback {
+	firstName: string;
+	lastName: string;
+	telNum: number;
+	email: string;
+	agree: boolean;
+	contactType: string;
+	message: string;
+};
+
+export const ContactType = ['None', 'Telephone', 'Email'];
