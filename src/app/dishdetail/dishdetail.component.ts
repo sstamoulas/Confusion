3176681d86486bec30b16a1e436dba57519d3e6b
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-03-21 14:49:42 +0300

    HTTP Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
@@ -39,7 +39,8 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishservice: DishService,
   			private route: ActivatedRoute,
   			private location: Location,
-        private fb: FormBuilder) { 
+        private fb: FormBuilder,
+        @Inject('BaseURL') private BaseURL) { 
     
     this.createForm();
   }
