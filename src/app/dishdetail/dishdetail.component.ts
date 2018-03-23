91cc7b9c538c5c668816335428df1b3b38c4c7ab
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2018-03-23 15:56:10 +0300

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,23 +5,39 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)', 
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
   commentForm: FormGroup;
   comment: Comment;
   dish: Dish;
-  dishcopy = null;
   dishIds: number[];
   prev: number;
   next: number;
   errMsg: string;
+
+  dishcopy = null;
+  visibility = 'shown';
   formErrors = {
     'author': '',
     'rating': '',
@@ -51,8 +67,16 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds()
         .subscribe(dishIds => this.dishIds = dishIds);
   	this.route.params
-        .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); }, 
+        .switchMap((params: Params) => { 
+          this.visibility = 'hidden'; 
+          return this.dishservice.getDish(+params['id']); 
+        })
+        .subscribe(dish => { 
+          this.dish = dish; 
+          this.dishcopy = dish; 
+          this.setPrevNext(dish.id); 
+          this.visibility = 'shown'; 
+        }, 
         errMsg => this.errMsg = <any>errMsg);
   }
 
